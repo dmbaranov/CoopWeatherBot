c4ab813f75ba8e746b113120ecb770ac7f877ce7
Dmitrii Baranov <me@dmbaranov.io>
2022-12-31 04:07:50 +0200

    Added missing hero job

--- lib/src/discord/bot.dart
+++ lib/src/discord/bot.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:nyxx/nyxx.dart';
 import 'package:nyxx_commands/nyxx_commands.dart';
+import 'package:cron/cron.dart';
 
 import 'package:weather/src/modules/swearwords_manager.dart';
 import 'package:weather/src/modules/reputation.dart';
@@ -46,11 +48,40 @@ class DiscordBot {
 
     // It was decided to disable weather notifications for now
     // _subscribeToWeather();
+
+    _startHeroCheckJob();
   }
 
   void _subscribeToWeather() {
     weather.weatherStream.listen((weatherString) {
       bot.httpEndpoints.sendMessage(Snowflake(channelId), MessageBuilder.content(weatherString));
+    });
+  }
+
+  void _startHeroCheckJob() async {
+    Cron().schedule(Schedule.parse('0 4 * * 6,0'), () async {
+      await Process.run('${Directory.current}/generate-online', []);
+
+      var onlineFile = File('assets/online');
+      var onlineUsers = await onlineFile.readAsLines();
+
+      if (onlineUsers.isEmpty) {
+        var message = sm.get('no_users_online_at_five');
+        return bot.httpEndpoints.sendMessage(Snowflake(channelId), MessageBuilder.content(message));
+      }
+
+      var heroesMessage = sm.get('users_online_at_five');
+
+      onlineUsers.forEach((userId) {
+        var onlineUser = users.firstWhere((user) => user.id == userId.toSnowflake());
+
+        heroesMessage += onlineUser.username;
+        heroesMessage += '\n';
+      });
+
+      await bot.httpEndpoints.sendMessage(Snowflake(channelId), MessageBuilder.content(heroesMessage));
+
+      await onlineFile.delete();
     });
   }
 

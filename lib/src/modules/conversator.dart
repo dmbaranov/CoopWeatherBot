584b31318fd30d24782fd0a23a020bd4fd2e936e
Dmitrii Baranov <me@dmbaranov.io>
2023-04-22 18:29:28 +0300

    Updated conversator model, passing previous messages to the conversation

--- lib/src/modules/conversator.dart
+++ lib/src/modules/conversator.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:weather/src/modules/database-manager/database_manager.dart';
+import 'package:weather/src/modules/database-manager/entities/conversator_chat_entity.dart';
 
 const String _converstorApiURL = 'https://api.openai.com/v1/chat/completions';
-const String _conversatorModel = 'gpt-3.5-turbo-0301';
+const String _conversatorModel = 'gpt-3.5-turbo';
 
 class Conversator {
   final DatabaseManager dbManager;
@@ -16,11 +17,13 @@ class Conversator {
   Future<String> getConversationReply(
       {required String chatId, required String parentMessageId, required String currentMessageId, required String message}) async {
     var conversationId = await getConversationId(chatId, parentMessageId);
+    var previousMessages = await dbManager.conversatorChat.getMessagesForConversation(chatId, conversationId);
     await saveConversationMessage(
         chatId: chatId, conversationId: conversationId, currentMessageId: currentMessageId, message: message, fromUser: true);
 
-    var response = 'hardcoded response';
-    return response;
+    var response = await _getConversatorResponse(previousMessages, message);
+
+    return response['choices']?[0]?['message']?['content'] ?? 'No response';
   }
 
   Future<void> saveConversationMessage(
@@ -38,14 +41,14 @@ class Conversator {
     return conversationId;
   }
 
-  Future<Map<String, dynamic>> _getConversatorResponse(String question) async {
+  Future<Map<String, dynamic>> _getConversatorResponse(List<ConversatorChatMessage> previousMessages, String question) async {
+    var formattedMessages = previousMessages
+        .map((message) => {'role': message.fromUser ? 'user' : 'system', 'content': message.message})
+        .toList()
+      ..add({'role': 'user', 'content': question});
+
     var headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer $conversatorApiKey'};
-    var body = {
-      'model': _model,
-      'messages': [
-        {'role': 'user', 'content': question}
-      ]
-    };
+    var body = {'model': _model, 'messages': formattedMessages};
 
     var response = await post(Uri.parse(_apiBaseUrl), headers: headers, body: json.encode(body), encoding: Encoding.getByName('utf-8'));
 

6db451e953a95f966fca63702860ed3987c48956
Dmitrii Baranov <me@dmbaranov.io>
2024-02-18 13:11:12 +0200

    Lint fixes

--- lib/src/core/repositories/check_reminder_repository.dart
+++ lib/src/core/repositories/check_reminder_repository.dart
@@ -2,7 +2,7 @@ import 'package:weather/src/core/check_reminder.dart' show CheckReminderData;
 import 'repository.dart';
 
 class CheckReminderRepository extends Repository {
-  CheckReminderRepository({required super.dbConnection}) : super(repositoryName: 'check_reminder') {}
+  CheckReminderRepository({required super.dbConnection}) : super(repositoryName: 'check_reminder');
 
   Future<int> createCheckReminder(String chatId, String userId, String message, DateTime timestamp) {
     return executeTransaction(

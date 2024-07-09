1ce00f0a5e8d10e41e283e24271df37be48d9d8c
Dmitrii Baranov <me@dmbaranov.io>
2024-07-09 23:03:48 +0300

    WIP. Moved more logic from manager to module, updated results structure

--- lib/src/modules/accordion_poll/accordion_poll_2.dart
+++ lib/src/modules/accordion_poll/accordion_poll_2.dart
@@ -1,26 +1,29 @@
+import 'package:weather/src/core/swearwords.dart';
 import 'package:weather/src/globals/poll.dart';
+import 'package:weather/src/globals/accordion_vote_option.dart';
+import 'package:weather/src/injector/injection.dart';
+
+typedef VoteOptionData = (AccordionVoteOption, String);
+
+const _accordionPollDuration = Duration(seconds: 180);
+const _accordionOptions = [
+  (AccordionVoteOption.yes, 'accordion.options.yes'),
+  (AccordionVoteOption.no, 'accordion.options.no'),
+  (AccordionVoteOption.maybe, 'accordion.options.maybe'),
+];
 
 class AccordionPoll2 extends Poll {
+  final Swearwords _sw;
   bool _pollActive = false;
-  Map<String, int> _pollResults = {};
+  Map<String, (AccordionVoteOption, int)> _pollResults = {};
   Duration _duration = Duration(seconds: 0);
   String? _fromUserId;
   String? _toUserId;
 
-  AccordionPoll2({required super.title, super.description});
+  AccordionPoll2({required super.title, super.description}) : _sw = getIt<Swearwords>();
 
   @override
-  String? get result {
-    var winOption = _pollResults.entries
-        .toList()
-        .reduce((currentOption, nextOption) => currentOption.value > nextOption.value ? currentOption : nextOption);
-
-    if (winOption.value > 0) {
-      return winOption.key;
-    }
-
-    return null;
-  }
+  String? get result => _winOption.value.$2 > 0 ? _winOption.key : null;
 
   @override
   Duration get duration => _duration;
@@ -28,19 +31,23 @@ class AccordionPoll2 extends Poll {
   @override
   get options => _pollResults.keys.toList();
 
-  Future<Poll> startPoll(
-      {required Duration duration, required List<String> options, required String fromUserId, required String toUserId}) async {
+  MapEntry<String, (AccordionVoteOption, int)> get _winOption => _pollResults.entries
+      .toList()
+      .reduce((currentOption, nextOption) => currentOption.value.$2 > nextOption.value.$2 ? currentOption : nextOption);
+
+  Future<Poll> startPoll({required String chatId, required String fromUserId, required String toUserId}) async {
     if (_pollActive) {
       // TODO: add other exceptions
       throw Exception('Poll active');
     }
 
-    options.forEach((option) {
-      _pollResults[option] = 0;
+    var translatedOptions = _getTranslatedOptions(chatId, _accordionOptions);
+    translatedOptions.forEach((option) {
+      _pollResults[option.$2] = (option.$1, 0);
     });
 
     _pollActive = true;
-    _duration = duration;
+    _duration = _accordionPollDuration;
     _fromUserId = fromUserId;
     _toUserId = toUserId;
 
@@ -49,6 +56,8 @@ class AccordionPoll2 extends Poll {
 
   @override
   void endPoll() {
+    print('Emitting option: ${_winOption.value.$1}');
+
     _pollActive = false;
     _pollResults = {};
     _fromUserId = null;
@@ -63,6 +72,10 @@ class AccordionPoll2 extends Poll {
       throw Exception("Option $option does not exist in this poll");
     }
 
-    _pollResults[option] = newOptionResult ?? pollOption + 1;
+    _pollResults[option] = (pollOption.$1, newOptionResult ?? pollOption.$2 + 1);
+  }
+
+  List<VoteOptionData> _getTranslatedOptions(String chatId, List<VoteOptionData> voteOptions) {
+    return voteOptions.map((option) => (option.$1, _sw.getText(chatId, option.$2))).toList();
   }
 }
--- lib/src/modules/accordion_poll/accordion_poll_manager_2.dart
+++ lib/src/modules/accordion_poll/accordion_poll_manager_2.dart
@@ -8,13 +8,6 @@ import 'package:weather/src/modules/modules_mediator.dart';
 import 'package:weather/src/modules/utils.dart';
 import 'accordion_poll_2.dart';
 
-const _accordionPollDuration = Duration(seconds: 180);
-const _accordionOptions = [
-  'accordion.options.yes',
-  'accordion.options.no',
-  'accordion.options.maybe',
-];
-
 class AccordionPollException extends ModuleException {
   AccordionPollException(super.cause);
 }
@@ -42,32 +35,11 @@ class AccordionPollManager2 implements ModuleManager {
 
     // TODO: move options to accordion_poll_2, use accordion_vote_option
     AccordionPoll2(title: _sw.getText(chatId, 'accordion.other.title'), description: _sw.getText(chatId, 'accordion.other.explanation'))
-        .startPoll(
-            duration: _accordionPollDuration, options: _translateOptions(chatId), fromUserId: event.userId, toUserId: event.otherUser!.id)
+        .startPoll(chatId: chatId, fromUserId: event.userId, toUserId: event.otherUser!.id)
         .then((poll) => platform.concludePoll(chatId, poll))
         .then((pollResult) =>
             sendOperationMessage(chatId, platform: platform, operationResult: true, successfulMessage: _getSuccessMessage(pollResult)))
         .catchError((error) => handleException(error, chatId, platform));
-
-    // var poll = AccordionPoll2(
-    //     title: _sw.getText(chatId, 'accordion.other.title'), description: _sw.getText(chatId, 'accordion.other.explanation'));
-    // var pollStarted = poll.startPoll(duration: _accordionPollDuration, options: _translateOptions(chatId));
-    //
-    // if (!pollStarted) {
-    //   throw Exception('__translate__ poll not started');
-    // }
-    //
-    // var result = await platform.concludePoll(chatId, poll);
-    //
-    // if (result == null) {
-    //   print('poll finished without results');
-    // } else {
-    //   print('poll result: $result');
-    // }
-  }
-
-  List<String> _translateOptions(String chatId) {
-    return _accordionOptions.map<String>((option) => _sw.getText(chatId, option)).toList();
   }
 
   String _getSuccessMessage(String? pollResult) {

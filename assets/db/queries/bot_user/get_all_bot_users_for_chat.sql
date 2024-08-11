371e93f0a766031340c581ab2da33c8751fbe58b
Dmitrii Baranov <me@dmbaranov.io>
2024-08-11 11:10:11 +0300

    Exclude deleted user when querying database

--- assets/db/queries/bot_user/get_all_bot_users_for_chat.sql
+++ assets/db/queries/bot_user/get_all_bot_users_for_chat.sql
@@ -1,4 +1,5 @@
 SELECT id, name, is_premium, deleted, banned, moderator
 FROM bot_user
          INNER JOIN chat_member cm ON bot_user.id = cm.bot_user_id
-WHERE chat_id = @chatId;
+WHERE chat_id = @chatId
+  AND deleted = false;
--- assets/db/queries/bot_user/get_single_user_for_chat.sql
+++ assets/db/queries/bot_user/get_single_user_for_chat.sql
@@ -2,4 +2,5 @@ SELECT id, name, is_premium, deleted, banned, moderator
 FROM bot_user
          INNER JOIN chat_member cm ON bot_user.id = cm.bot_user_id
 WHERE chat_id = @chatId
-  AND bot_user.id = @userId;
+  AND bot_user.id = @userId
+  AND deleted = false;

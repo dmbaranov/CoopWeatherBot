5d2555f8ebecf4fc705a545184245854e5f1abf5
Dmitrii Baranov <me@dmbaranov.io>
2024-11-04 18:07:00 +0200

    Filter out deleted users from chat reputation

--- assets/db/queries/reputation/get_reputation_for_chat.sql
+++ assets/db/queries/reputation/get_reputation_for_chat.sql
@@ -1,5 +1,8 @@
-SELECT bot_user.name, reputation.reputation, bot_user
+SELECT bot_user.name, reputation.reputation
 FROM reputation
          INNER JOIN bot_user ON reputation.bot_user_id = bot_user.id
+         INNER JOIN chat_member
+                    ON reputation.chat_id = chat_member.chat_id AND reputation.bot_user_id = chat_member.bot_user_id
 WHERE reputation.chat_id = @chatId
+  AND chat_member.deleted = FALSE
 ORDER BY reputation;

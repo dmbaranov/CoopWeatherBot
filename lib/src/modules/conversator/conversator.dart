e41cf925e3cf92f0c0cae9cb5365bbcf641c37f7
Dmitrii Baranov <me@dmbaranov.io>
2025-08-10 13:35:04 +0300

    Updated models for conversator

--- lib/src/modules/conversator/conversator.dart
+++ lib/src/modules/conversator/conversator.dart
@@ -11,8 +11,8 @@ import 'package:weather/src/globals/module_exception.dart';
 import 'package:weather/src/utils/logger.dart';
 
 const String _converstorApiURL = 'https://api.openai.com/v1/chat/completions';
-const String regularModel = 'gpt-4.1-mini';
-const String advancedModel = 'gpt-4.1';
+const String regularModel = 'gpt-5-mini';
+const String advancedModel = 'gpt-5';
 const int regularDailyLimit = 100;
 const int advancedDailyLimit = 10;
 
